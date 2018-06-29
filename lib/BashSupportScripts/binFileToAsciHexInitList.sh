e6f56465c94cf4fdc3ce592754b41fda0a87c669
Christian Löpke <loepke@edfritsch.de>
2018-06-29 18:35:18 +0200

    Added conversion to (char) to suppress implicit conversion int to char warning, which is known and safe.

--- lib/BashSupportScripts/binFileToAsciHexInitList.sh
+++ lib/BashSupportScripts/binFileToAsciHexInitList.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat public_key.bin | hexdump -v -e '"0x" 1/1 "%02X" ", "' | rev | cut -c 3- | rev ; echo
+cat public_key.bin | hexdump -v -e '"(char)0x" 1/1 "%02X" ", "' | rev | cut -c 3- | rev ; echo

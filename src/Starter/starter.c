da2e8f4b27a8e7d3413545bc18bced9f3f6e37c9
Christian Löpke <loepke@edfritsch.de>
2018-07-22 18:41:26 +0200

    Added a spaghetti draft version test for parameter injection.

--- src/Starter/starter.c
+++ src/Starter/starter.c
@@ -3,13 +3,21 @@
 #include <tchar.h>
 #include <string>
 #include <sstream>
+#include <psapi.h>
 
 #define EXECPATHMAXLENGTH 1024
 
 using namespace std;
 
-bool injectDll(int procID, HANDLE hThread, const char *dllPath)
+string openGMPLibName = "OpenGMP.dll";
+string serverAddress = "127.0.0.1";
+unsigned short serverPort = 1760;
+
+bool injectDll(PROCESS_INFORMATION *pi, const char *dllPath)
 {
+	DWORD procID = pi->dwProcessId;
+	HANDLE hThread = pi->hThread;
+
 	HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procID);
 	if(process == NULL) {
 		printf("Error: the specified process couldn't be found.\n");
@@ -50,9 +58,96 @@ bool injectDll(int procID, HANDLE hThread, const char *dllPath)
 	else {
 		printf("Success: the remote thread was successfully created.\n");
 	}
+
+	Sleep(1000); //Give the injected dll a chance, to get loaded.
+	HMODULE hMods[1024];
+	HMODULE hOpenGMP;
+    DWORD cbNeeded;
+    unsigned int i;
+	if( EnumProcessModules(process, hMods, sizeof(hMods), &cbNeeded))
+    	{
+	        for ( int i = 0; i < (cbNeeded / sizeof(HMODULE)); i++ )
+        	{
+	            TCHAR szModName[MAX_PATH];
+
+            	// Get the full path to the module's file.
+
+	            if ( GetModuleFileNameEx( process, hMods[i], szModName,
+    	                                  sizeof(szModName) / sizeof(TCHAR)))
+            {
+                // Print the module name and handle value.
+               	// _tprintf( TEXT("\t%s (0x%08X)\n"), szModName, hMods[i] );
+				std::string modName(szModName);				
+				if(modName.find(openGMPLibName) != string::npos)
+				{
+					printf("Found OpenGMP Module!\n");
+					hOpenGMP = hMods[i];
+					break;
+				}
+            }
+        }
+    }
+
+	HMODULE myOGMPModule = LoadLibraryEx(openGMPLibName.c_str(), NULL, DONT_RESOLVE_DLL_REFERENCES);
+	if(myOGMPModule == NULL)
+	{
+		printf("Cannot load OpenGMP.dll!\n");
+		return false;
+	}
+
+	char * mySetConnectDataAddr = (char*)GetProcAddress(myOGMPModule, "SetConnectData");
+	if(mySetConnectDataAddr == NULL)
+	{
+		printf("Cannot find SetConnectData Function in my OpenGMP.dll!\n");
+		return false;
+	}
+
+	char* injSetConnectDataAddr = (char*)mySetConnectDataAddr - (char*)myOGMPModule + (char*)hOpenGMP;
+
 	/*
-	* Close the handle to the process, becuase we've already injected the DLL.
+	* Allocate new memory region inside the process's address space.
 	*/
+	LPVOID injServerAddr = (LPVOID)VirtualAllocEx(process, NULL, strlen(serverAddress.c_str()), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+	if(injServerAddr == NULL) {
+		printf("Error: the memory could not be allocated inside the chosen process.\n");
+		return false;
+	}
+	/*
+	* Write the argument to LoadLibraryA to the process's newly allocated memory region.
+	*/
+	n = WriteProcessMemory(process, injServerAddr, serverAddress.c_str(), strlen(serverAddress.c_str()), NULL);
+	if(n == 0) {
+		printf("Error: there was no bytes written to the process's address space.\n");
+		return false;	
+	}
+
+/*
+	* Allocate new memory region inside the process's address space.
+	
+	LPVOID injServerPort = (LPVOID)VirtualAllocEx(process, NULL, sizeof(unsigned short), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+	if(injServerPort == NULL) {
+		printf("Error: the memory could not be allocated inside the chosen process.\n");
+		return false;
+	}
+	
+	* Write the argument to LoadLibraryA to the process's newly allocated memory region.
+	
+	n = WriteProcessMemory(process, injServerPort, &serverPort, sizeof(unsigned short), NULL);
+	if(n == 0) {
+		printf("Error: there was no bytes written to the process's address space.\n");
+		return false;
+	}
+*/
+
+	HANDLE threadID2 = CreateRemoteThread(process, NULL, 0, (LPTHREAD_START_ROUTINE)injSetConnectDataAddr, injServerAddr, NULL, NULL);
+	if(threadID2 == NULL) {
+		printf("Error: the remote thread could not be created.\n");
+		return false;
+	}
+	else {
+		printf("Success: the remote thread was successfully created.\n");
+	}
+
 	ResumeThread(hThread);
 	CloseHandle(process);
 	return true;
@@ -115,7 +210,7 @@ int main(int argc, char **argv)
 
 	DWORD result = WaitForInputIdle(pi.hProcess, 5000);
 
-	injectDll((int)pi.dwProcessId, pi.hThread, cinjectDllFullpath);
+	injectDll(&pi, cinjectDllFullpath);
 
 	// Wait until child process exits.
 	WaitForSingleObject( pi.hProcess, INFINITE );

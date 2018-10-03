8da3f722d8cb5d7e305db13a518e8fc2539e617f
Christian Löpke <loepke@edfritsch.de>
2018-10-03 16:32:14 +0200

    Implemented environment based hostname/port handover to gothic process.

--- src/Starter/starter.c
+++ src/Starter/starter.c
@@ -1,17 +1,36 @@
+#include "../Shared/Types/constants.hpp"
 #include <windows.h>
 #include <stdio.h>
 #include <tchar.h>
 #include <string>
 #include <sstream>
 #include <psapi.h>
+#include <stdlib.h> //putenv
 
 #define EXECPATHMAXLENGTH 1024
 
 using namespace std;
+using namespace OpenGMP;
 
 string openGMPLibName = "OpenGMP.dll";
 string serverAddress = "127.0.0.1";
 unsigned short serverPort = 1760;
+
+bool setConnectDataEnvVars()
+{
+	string hostEnvVar = hostnameEnvVarName;
+	string portEnvVar = portEnvVarName;
+
+	hostEnvVar += "=";
+	hostEnvVar += serverAddress;
+	portEnvVar += "=";
+	portEnvVar += to_string(serverPort);
+
+	putenv(hostEnvVar.c_str());
+	putenv(portEnvVar.c_str());
+
+	return true;
+}
 
 bool injectDll(PROCESS_INFORMATION *pi, const char *dllPath)
 {
@@ -23,6 +42,7 @@ bool injectDll(PROCESS_INFORMATION *pi, const char *dllPath)
 		printf("Error: the specified process couldn't be found.\n");
 		return false;
 	}
+	
 	/*
 	* Get address of the LoadLibrary function.
 	*/
@@ -31,6 +51,7 @@ bool injectDll(PROCESS_INFORMATION *pi, const char *dllPath)
 		printf("Error: the LoadLibraryA function was not found inside kernel32.dll library.\n");
 		return false;
 	}
+	
 	/*
 	* Allocate new memory region inside the process's address space.
 	*/
@@ -39,6 +60,7 @@ bool injectDll(PROCESS_INFORMATION *pi, const char *dllPath)
 		printf("Error: the memory could not be allocated inside the chosen process.\n");
 		return false;
 	}
+	
 	/*
 	* Write the argument to LoadLibraryA to the process's newly allocated memory region.
 	*/
@@ -47,6 +69,7 @@ bool injectDll(PROCESS_INFORMATION *pi, const char *dllPath)
 		printf("Error: there was no bytes written to the process's address space.\n");
 		return false;	
 	}
+	
 	/*
 	* Inject our DLL into the process's address space.
 	*/
@@ -58,96 +81,7 @@ bool injectDll(PROCESS_INFORMATION *pi, const char *dllPath)
 	else {
 		printf("Success: the remote thread was successfully created.\n");
 	}
-
-	Sleep(1000); //Give the injected dll a chance, to get loaded.
-	HMODULE hMods[1024];
-	HMODULE hOpenGMP;
-    DWORD cbNeeded;
-    unsigned int i;
-	if( EnumProcessModules(process, hMods, sizeof(hMods), &cbNeeded))
-    	{
-	        for ( int i = 0; i < (cbNeeded / sizeof(HMODULE)); i++ )
-        	{
-	            TCHAR szModName[MAX_PATH];
-
-            	// Get the full path to the module's file.
-
-	            if ( GetModuleFileNameEx( process, hMods[i], szModName,
-    	                                  sizeof(szModName) / sizeof(TCHAR)))
-            {
-                // Print the module name and handle value.
-               	// _tprintf( TEXT("\t%s (0x%08X)\n"), szModName, hMods[i] );
-				std::string modName(szModName);				
-				if(modName.find(openGMPLibName) != string::npos)
-				{
-					printf("Found OpenGMP Module!\n");
-					hOpenGMP = hMods[i];
-					break;
-				}
-            }
-        }
-    }
-
-	HMODULE myOGMPModule = LoadLibraryEx(openGMPLibName.c_str(), NULL, DONT_RESOLVE_DLL_REFERENCES);
-	if(myOGMPModule == NULL)
-	{
-		printf("Cannot load OpenGMP.dll!\n");
-		return false;
-	}
-
-	char * mySetConnectDataAddr = (char*)GetProcAddress(myOGMPModule, "SetConnectData");
-	if(mySetConnectDataAddr == NULL)
-	{
-		printf("Cannot find SetConnectData Function in my OpenGMP.dll!\n");
-		return false;
-	}
-
-	char* injSetConnectDataAddr = (char*)mySetConnectDataAddr - (char*)myOGMPModule + (char*)hOpenGMP;
-
-	/*
-	* Allocate new memory region inside the process's address space.
-	*/
-	LPVOID injServerAddr = (LPVOID)VirtualAllocEx(process, NULL, strlen(serverAddress.c_str()), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-	if(injServerAddr == NULL) {
-		printf("Error: the memory could not be allocated inside the chosen process.\n");
-		return false;
-	}
-	/*
-	* Write the argument to LoadLibraryA to the process's newly allocated memory region.
-	*/
-	n = WriteProcessMemory(process, injServerAddr, serverAddress.c_str(), strlen(serverAddress.c_str()), NULL);
-	if(n == 0) {
-		printf("Error: there was no bytes written to the process's address space.\n");
-		return false;	
-	}
-
-/*
-	* Allocate new memory region inside the process's address space.
 	
-	LPVOID injServerPort = (LPVOID)VirtualAllocEx(process, NULL, sizeof(unsigned short), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-	if(injServerPort == NULL) {
-		printf("Error: the memory could not be allocated inside the chosen process.\n");
-		return false;
-	}
-	
-	* Write the argument to LoadLibraryA to the process's newly allocated memory region.
-	
-	n = WriteProcessMemory(process, injServerPort, &serverPort, sizeof(unsigned short), NULL);
-	if(n == 0) {
-		printf("Error: there was no bytes written to the process's address space.\n");
-		return false;
-	}
-*/
-
-	HANDLE threadID2 = CreateRemoteThread(process, NULL, 0, (LPTHREAD_START_ROUTINE)injSetConnectDataAddr, injServerAddr, NULL, NULL);
-	if(threadID2 == NULL) {
-		printf("Error: the remote thread could not be created.\n");
-		return false;
-	}
-	else {
-		printf("Success: the remote thread was successfully created.\n");
-	}
-
 	ResumeThread(hThread);
 	CloseHandle(process);
 	return true;
@@ -190,6 +124,8 @@ int main(int argc, char **argv)
 	strcpy(cinjectDllFullpath, parser.str().c_str());
 	parser.str(string("")); //Clear parser
 	parser.clear();		//Clear parsers error flags
+
+	setConnectDataEnvVars(); //Set connect data environment variables.
 
 	// Start the child process.
 	if( !CreateProcess( 

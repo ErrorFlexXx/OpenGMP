4cc47df59e29413ba5ed616a8d02605ed6ab0fc1
Christian Löpke <loepke@edfritsch.de>
2018-11-18 12:04:19 +0100

    Checking missing string type class.

---
+++ src/Shared/Types/string.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <Shared/Objects/iStreamObject.hpp>
+#include <string>
+#include <BitStream.h>
+#include <RakNetTypes.h>
+#include <ostream>
+
+namespace OpenGMP
+{
+    /**
+     * @brief The String component
+     */
+    class String : public IStreamObject
+    {
+    public:
+        String();
+        String(const std::string &value);
+        String(const char *value);
+
+        operator std::string() { return text; }
+        String &operator<< (float arg);
+        String &operator<< (const String &arg);
+        std::string text;
+
+        void WriteStream(RakNet::BitStream &stream) const;
+        bool ReadStream(RakNet::BitStream &stream);
+    };
+}
+
+//Support shifting in ostream (used in logger impl.)
+std::ostream & operator<< (std::ostream &out, const OpenGMP::String &t);
